49e09e17fc7c2eed149ae0b6db9e98e1b60daa40
Juan Cabrera <yesuscabrera2003@gmail.com>
2025-03-20 19:07:15 -0600

    data and table in home.html

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ import pandas
 
 app = Flask(__name__)
 
+stations = pandas.read_csv("data_small/stations.txt", skiprows=17)
+stations = stations[['STAID','STANAME                                 ']]
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    data = pandas.read_csv("")
-    temperature = 23
-    return {"station: " station,
-            "date: " date,
-            "temperature: ", temperature}
+    filepath = "data_small\TG_STAID" + str(station).zfill(6) + ".txt"
+    data = pandas.read_csv(filepath, skiprows=20, parse_dates=['    DATE'])
+    temperature = data.loc[data['    DATE']== date]['   TG'].squeeze() / 10
+    return {"station": station,
+            "date ": date,
+            "temperature ": temperature}
 
 
 if __name__ =="__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
